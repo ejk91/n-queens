e8d3d985ebb2c917c154f64c9a1cf18fdf64481f
ejk91 <enochkim91@gmail.com>
2017-02-15 17:32:27 -0800

    Completes row conflicts, column conflicts, given index major diagonal.

--- src/Board.js
+++ src/Board.js
@@ -79,12 +79,40 @@
     //
     // test if a specific row on this board contains a conflict
     hasRowConflictAt: function(rowIndex) {
-      return false; // fixme
+      //get the desired row / set up count
+      var row = this.get(rowIndex);
+
+      var count = 0;
+      // go through each index of the row
+      for (var i = 0; i < row.length; i++) {
+      // find sum of row
+        count += row[i];
+      }
+      // if count > 1
+      //console.log('this is the row ', row , 'this is our test ' , count > 1);
+      return (count > 1);
+        // return false
+      // else return true
+      //return false; // fixme
+
     },
 
     // test if any rows on this board contain conflicts
     hasAnyRowConflicts: function() {
-      return false; // fixme
+      // access the whole board
+      // how do we access the whole board? if we know the size we can access all the rows
+      var size = this.get('n');
+      // console.log('this is our size', size);
+      // loop through each row to see if it has conflict
+      for (var i = 0; i < size; i++) {
+        // if row has no conflict continue - this is where we use hasRowConflictAt
+        // if row has conflict return false
+        if (this.hasRowConflictAt(i)) {
+          return true;
+        }
+      }
+      return false;
+      //return false; // fixme
     },
 
 
@@ -94,11 +122,39 @@
     //
     // test if a specific column on this board contains a conflict
     hasColConflictAt: function(colIndex) {
-      return false; // fixme
+      //get number of columns, which is n
+      var size = this.get('n');
+      // make count variable
+      var count = 0;
+
+      // loop through each row
+      for (var i = 0; i < size; i++) {
+      // add index zero of current row to count
+        var row = this.get(i);
+        count += row[colIndex];
+      }
+
+      // if count greater than 1 
+      return (count > 1);
+        //return true
+
+
+      // if not return false; // fixme
+
     },
 
     // test if any columns on this board contain conflicts
     hasAnyColConflicts: function() {
+      // find number of columns
+      var columns = this.get('n');
+      // loop through each columns to see if it has conflict
+      for (var i = 0; i < columns; i++) {
+        if (this.hasColConflictAt(i)) {
+          return true;
+        }
+      }
+      //  if it has conflict
+        // return true
       return false; // fixme
     },
 
@@ -109,11 +165,48 @@
     //
     // test if a specific major diagonal on this board contains a conflict
     hasMajorDiagonalConflictAt: function(majorDiagonalColumnIndexAtFirstRow) {
-      return false; // fixme
+      //get the size of the board
+      var cols = this.get('n');
+      //counter for number of instances
+      var count = 0;
+      //console.log('this is the first index ', majorDiagonalColumnIndexAtFirstRow );
+      //iterate over each row
+      if (majorDiagonalColumnIndexAtFirstRow >= 0) {
+        for (var i = 0; i < cols - majorDiagonalColumnIndexAtFirstRow; i++ ) {  // makes sure we dont add undefined      
+          //access the index + 1 on each row
+          var row = this.get(i);
+          count += row[majorDiagonalColumnIndexAtFirstRow + i];
+           //if index + 1 is greater than one
+            // return true
+            // return false
+        } 
+        return (count > 1);
+      }
+      if (majorDiagonalColumnIndexAtFirstRow < 0) {
+        var startAt = Math.abs(majorDiagonalColumnIndexAtFirstRow);
+        //console.log('This is our input' ,majorDiagonalColumnIndexAtFirstRow, 'This is where we start', startAt);
+        for (var j = 0; j < cols - startAt; j++) {
+          var row = this.get(startAt + i);
+          count += row[i];
+
+        }
+        return (count > 1);
+      }
+      // console.log('this is the row ', row , 'this is our test ' , count > 1);
     },
 
     // test if any major diagonals on this board contain conflicts
     hasAnyMajorDiagonalConflicts: function() {
+      //get board size
+      //begin at row zero and col zero and iterate over the board
+   
+
+      //add a sum
+        //if first value is equal to one add to count
+          //move down one column and over one index
+          //add an iteration lenght that shortens by one
+
+
       return false; // fixme
     },
 
